f339048e302c1380a14de2218fe0a2249f57d18d
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2020-12-06 20:14:54 +0700

    test

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -18,3 +18,5 @@ print(type('Asabeneh'))          # String
 print(type([1, 2, 3]))           # List
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Tuple
+
+print("test")
